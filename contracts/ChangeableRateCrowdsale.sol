19b566559110a80d36d8ac3a28e5c76308fba334
DApp Dev <d.degterin@gmail.com>
2017-11-23 21:16:50 +0000

    Changeable Rate Crowdsale and Platform prototype

---
+++ contracts/ChangeableRateCrowdsale.sol
@@ -0,0 +1,108 @@
+pragma solidity ^0.4.11;
+
+import './ClaimableCrowdsale.sol';
+
+/**
+ * @title ChangeableRateCrowdsale
+ * @dev HoQu.io Main Sale stage
+ */
+contract ChangeableRateCrowdsale is ClaimableCrowdsale {
+
+    struct RateBoundary {
+        uint256 amount;
+        uint256 rate;
+    }
+
+    mapping (uint => RateBoundary) public rateBoundaries;
+    uint public currentBoundary = 0;
+    uint public numOfBoundaries = 0;
+    uint256 public nextBoundaryAmount;
+
+    /**
+    * @param _tokenAddress address of a HQX token contract
+    * @param _bankAddress address for remain HQX tokens accumulation
+    * @param _beneficiaryAddress accepted ETH go to this address
+    * @param _tokenRate rate HQX per 1 ETH
+    * @param _minBuyableAmount min ETH per each buy action (in ETH wei)
+    * @param _maxTokensAmount ICO HQX capacity (in HQX wei)
+    * @param _endDate the date when ICO will expire
+    */
+    function ChangeableRateCrowdsale(
+        address _tokenAddress,
+        address _bankAddress,
+        address _beneficiaryAddress,
+        uint256 _tokenRate,
+        uint256 _minBuyableAmount,
+        uint256 _maxTokensAmount,
+        uint256 _endDate
+    ) ClaimableCrowdsale(
+         _tokenAddress,
+         _bankAddress,
+         _beneficiaryAddress,
+         _tokenRate,
+         _minBuyableAmount,
+         _maxTokensAmount,
+         _endDate
+    ) {
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 13777764 ether,
+            rate: 6000
+        });
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 27555528 ether,
+            rate: 5750
+        });
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 41333292 ether,
+            rate: 5650
+        });
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 55111056 ether,
+            rate: 5550
+        });
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 68888820 ether,
+            rate: 5450
+        });
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 82666584 ether,
+            rate: 5350
+        });
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 96444348 ether,
+            rate: 5250
+        });
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 110222112 ether,
+            rate: 5150
+        });
+        rateBoundaries[numOfBoundaries++] = RateBoundary({
+            amount: 137777640 ether,
+            rate: 5000
+        });
+        nextBoundaryAmount = rateBoundaries[currentBoundary].amount;
+    }
+
+    /**
+     * Internal method to change rate if boundary is hit
+     */
+    function touchRate() internal {
+        if (issuedTokensAmount >= nextBoundaryAmount) {
+            currentBoundary++;
+            if (currentBoundary >= numOfBoundaries) {
+                nextBoundaryAmount = maxTokensAmount;
+            } else {
+                nextBoundaryAmount = rateBoundaries[currentBoundary].amount;
+                tokenRate = rateBoundaries[currentBoundary].rate;
+            }
+        }
+    }
+
+    /**
+     * Inherited internal method for storing tokens in contract until claim stage
+     */
+    function storeTokens(address _receiver, uint256 _tokensAmount) internal whenNotPaused {
+        ClaimableCrowdsale.storeTokens(_receiver, _tokensAmount);
+        touchRate();
+    }
+}
--- contracts/HoQuPlatform.sol
+++ contracts/HoQuPlatform.sol
@@ -1,207 +1,184 @@
 pragma solidity ^0.4.17;
 
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-import './HoQuToken.sol';
 import './HoQuPlatformConfig.sol';
+import './HoQuToken.sol';
 
 contract HoQuPlatform {
-    using SafeMath for uint256;
-    
-    uint8 constant STATUS_NOT_EXIST = 0;
-    uint8 constant STATUS_CREATED = 1;
-    uint8 constant STATUS_PENDING = 2;
-    uint8 constant STATUS_PROCESSING = 3; //really need it?
-    uint8 constant STATUS_APPROVED = 4;
-    uint8 constant STATUS_ACTIVE = 5;
-    uint8 constant STATUS_DONE = 6; // == approved?
-    uint8 constant STATUS_DECLINED = 7;
-    uint8 constant STATUS_APPEALING = 8;
+	using SafeMath for uint256;
 
-    HoQuPlatformConfig public config;
+	enum Status { NotExists, Created, Pending, Active, Declined }
+    enum KycLevel { Tier0, Tier1, Tier2, Tier3, Tier4 }
 
-    HoQuToken public token;
+    struct KycReport {
+        uint createdAt;
+        uint32 reportId;
+        KycLevel kycLevel;
+        string dataUrl;
+    }
 
-    // HoQu platform commission in szabo = 0.5 szabo
-    uint256 public commission;
+    struct User {
+        uint createdAt;
+        mapping (uint8 => address) addresses;
+        uint8 numOfAddresses;
+        string role;
+        KycLevel kycLevel;
+        mapping (uint16 => KycReport) kycReports;
+        uint16 numOfKycReports;
+        string pubKey;
+        Status status;
+    }
 
-    struct Network {
-        address owner;
+	struct Company {
+		uint ownerId;
+        address ownerAddress;
+		string name;
+		string dataUrl;
+		Status status;
+	}
+
+	struct Offer {
+        uint createdAt;
+		uint companyId;
+        address payerAddress;
+		string name;
+		string dataUrl;
+		uint256 cost;
+		Status status;
+	}
+
+    struct Tracker {
+        address ownerAddress;
         string name;
-        string url;
-        uint8 status;
+        string dataUrl;
     }
 
-    struct Offer {
-        address owner;
-        string networkId;
-        string name;
-        string url;
-        uint256 cost;
-        uint8 status;
+	struct Lead {
+        uint createdAt;
+        uint trackerId;
+        uint ownerId;
+        address beneficiaryAddress;
+		uint offerId;
+		string dataUrl;
+		string meta;
+		uint256 price;
+        mapping (address => uint32) intermediaries;
+		Status status;
+	}
+
+	HoQuPlatformConfig public config;
+	HoQuToken public token;
+
+    mapping(uint32 => User) public users;
+	mapping(uint32 => Company) public companies;
+	mapping(uint32 => Offer) public offers;
+    mapping(uint32 => Tracker) public trackers;
+	mapping(uint32 => Lead) public leads;
+
+    event UserRegistered(address indexed ownerAddress, uint32 id, string role);
+    event UserAddressAdded(address indexed ownerAddress, address additionalAddress);
+    event UserPubKeyUpdated(address indexed ownerAddress);
+    event UserKycReportAdded(address indexed ownerAddress, KycLevel kycLevel);
+    event UserStatusChanged(address indexed ownerAddress, Status status);
+
+    modifier onlyOwner() {
+        require(msg.sender == config.systemOwner());
+        _;
     }
 
-    struct Lead {
-        address owner;
-        string offerId;
-        string url;
-        string secret;
-        string meta;
-        uint256 price;
-        uint8 status;
-    }
+	function HoQuPlatform(
+        address configAddress,
+        address tokenAddress
+    ) public {
+		config = HoQuPlatformConfig(configAddress);
+		token = HoQuToken(tokenAddress);
+	}
 
-    mapping(string => Network) networks;
-    mapping(string => Offer) offers;
-    mapping(string => Lead) leads;
-    
-    event NetworkActivated(string id);
-    event OfferActivated(string id);
-    event LeadAdded(string id, string offerId, string meta);
-    event LeadApproved(string id);
-    event LeadDeclined(string id);
-
-    function HoQuPlatform(address configAddress, address tokenAddress, uint256 _commission) public {
+    function setConfigAddress(address configAddress) public onlyOwner {
         config = HoQuPlatformConfig(configAddress);
-        token = HoQuToken(tokenAddress);
-        commission = _commission;
     }
 
-    function addNetwork(string id, string name, string url) public returns (bool) {
-        require (bytes(id).length != 0 && networks[id].status == STATUS_NOT_EXIST);
-    
-        networks[id] = Network({
-            owner: msg.sender,
-            name: name,
-            url: url,
-            status: STATUS_CREATED
+    function registerUser(uint32 id, string role, address ownerAddress, string pubKey) public onlyOwner returns (bool) {
+        require (users[id].status == Status.NotExists);
+
+        users[id] = User({
+            createdAt: now,
+            numOfAddresses: 1,
+            role: role,
+            kycLevel: KycLevel.Tier0,
+            numOfKycReports: 0,
+            pubKey: pubKey,
+            status: Status.Created
         });
-    
+        users[id].addresses[0] = ownerAddress;
+
+        UserRegistered(ownerAddress, id, role);
+
         return true;
     }
 
-    function activateNetwork(string id) public returns (bool) {
-        require (msg.sender == config.systemOwner());
-        require (bytes(id).length != 0 && networks[id].status != STATUS_NOT_EXIST);
-    
-        networks[id].status = STATUS_ACTIVE;
-    
-        NetworkActivated(id);
-    
+    function addUserAddress(uint32 id, address ownerAddress) public onlyOwner returns (bool) {
+        require (users[id].status != Status.NotExists);
+
+        users[id].addresses[users[id].numOfAddresses] = ownerAddress;
+        users[id].numOfAddresses++;
+
+        UserAddressAdded(users[id].addresses[0], ownerAddress);
+
         return true;
     }
-    
-    function getNetwork(string id) public returns (string, string, uint8) {
-        require (bytes(id).length != 0 && networks[id].status != STATUS_NOT_EXIST);
-    
-        return (networks[id].name, networks[id].url, networks[id].status);
+
+    function updateUserPubKey(uint32 id, string pubKey) public onlyOwner returns (bool) {
+        require (users[id].status != Status.NotExists);
+
+        users[id].pubKey = pubKey;
+
+        UserPubKeyUpdated(users[id].addresses[0]);
+
+        return true;
     }
-    
-    function addOffer(string id, string networkId, string name, string url, uint256 cost) public returns (bool) {
-        require (bytes(id).length != 0 && offers[id].status == STATUS_NOT_EXIST);
-    
-        require (bytes(networkId).length != 0 && networks[networkId].status == STATUS_ACTIVE);
-    
-        offers[id] = Offer({
-            owner: msg.sender,
-            networkId: networkId,
-            name: name,
-            url: url,
-            cost: cost,
-            status: STATUS_CREATED
+
+    function addUserKycReport(uint32 id, uint32 reportId, KycLevel kycLevel, string dataUrl) public onlyOwner returns (bool) {
+        require (users[id].status != Status.NotExists);
+
+        users[id].kycReports[users[id].numOfKycReports] = KycReport({
+            createdAt: now,
+            reportId: reportId,
+            kycLevel: kycLevel,
+            dataUrl: dataUrl
         });
-    
-        return true;
-    }
-    
-    function activateOffer(string id) public returns (bool) {
-        require (msg.sender == config.systemOwner());
-        require (bytes(id).length != 0 && offers[id].status != STATUS_NOT_EXIST);
-        
-        offers[id].status = STATUS_ACTIVE;
-        
-        OfferActivated(id);
-    
-        return true;
-    }
-    
-    function getOffer(string id) public returns (string, string, string, uint256, uint8) {
-        require (bytes(id).length != 0 && offers[id].status != STATUS_NOT_EXIST);
-        
-        return (offers[id].networkId, offers[id].name, offers[id].url, offers[id].cost, offers[id].status);
-    }
-    
-    function addLead(string id, string offerId, string url, string secret, string meta) public returns (bool) {
-        require (bytes(id).length != 0 && leads[id].status == STATUS_NOT_EXIST);
-        
-        require (bytes(offerId).length != 0 && offers[offerId].status == STATUS_ACTIVE);
-    
-        leads[id] = Lead({
-            owner: msg.sender,
-            offerId: offerId,
-            url: url,
-            secret: secret,
-            meta: meta,
-            price: 0,
-            status: STATUS_PENDING
-        });
-        
-        LeadAdded(id, offerId, meta);
-        
-        return true;
-    }
+        users[id].numOfKycReports++;
+        users[id].kycLevel = kycLevel;
 
-    function approveLead(string id) public returns (bool) {
-        require (bytes(id).length != 0 && leads[id].status == STATUS_PENDING);
-
-        Lead lead = leads[id];
-        Offer offer = offers[lead.offerId];
-        
-        require (offer.owner == msg.sender && offer.status == STATUS_ACTIVE);
-
-        // all amounts are in weis
-        uint256 commissionAmount = offer.cost.mul(commission).mul(1 szabo).div(1 ether);
-        require(commissionAmount > 0); // assert instead of require?
-        uint256 amount = offer.cost.sub(commissionAmount);
-        // // todo: check allowance first
-        token.transferFrom(offer.owner, this, offer.cost);
-        token.transfer(config.commissionWallet(), commissionAmount);
-        token.transfer(lead.owner, amount);
-
-        leads[id].price = amount;
-        leads[id].status = STATUS_APPROVED;
-        
-        LeadApproved(id);
+        UserKycReportAdded(users[id].addresses[0], kycLevel);
 
         return true;
     }
-    
-    function declineLead(string id) public returns (bool) {
-        require (bytes(id).length != 0 && leads[id].status == STATUS_PENDING);
-        
-        Lead lead = leads[id];
-        Offer offer = offers[lead.offerId];
-        
-        require (offer.owner == msg.sender && offer.status == STATUS_ACTIVE);
-    
-        leads[id].status = STATUS_DECLINED;
-        
-        LeadDeclined(id);
-        
+
+    function setUserStatus(uint32 id, Status status) public onlyOwner returns (bool) {
+        require (users[id].status != Status.NotExists);
+
+        users[id].status = status;
+
+        UserStatusChanged(users[id].addresses[0], status);
+
         return true;
     }
-    
-    function getLead(string id) public returns (string, string, string, uint256, uint8) {
-        require (bytes(id).length != 0 && leads[id].status != STATUS_NOT_EXIST);
-        
-        return (leads[id].offerId, leads[id].url, leads[id].meta, leads[id].price, leads[id].status);
+
+    function getUserAddress(uint32 id, uint8 num) public constant returns (address) {
+        require (users[id].status != Status.NotExists);
+
+        return users[id].addresses[num];
     }
-    
-    function getLeadSecret(string id) public returns (string) {
-        require (bytes(id).length != 0 && leads[id].status == STATUS_APPROVED);
-        
-        Offer offer = offers[leads[id].offerId];
-        require (msg.sender == offer.owner);
-        
-        return leads[id].secret;
+
+    function getUserKycReport(uint32 id, uint16 num) public constant returns (uint, uint32, KycLevel, string) {
+        require (users[id].status != Status.NotExists);
+
+        return (
+            users[id].kycReports[num].createdAt,
+            users[id].kycReports[num].reportId,
+            users[id].kycReports[num].kycLevel,
+            users[id].kycReports[num].dataUrl
+        );
     }
 }
